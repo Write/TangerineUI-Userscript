7004dc710a8303b14b6faaf390ded8603a592963
Write <541722+Write@users.noreply.github.com>
2025-07-06 19:18:13 +0000

    Fix custom.css detection for >= 4.4.0. Enhance version detection to detect all pattern above 4.3.X correctly

--- TangerineUI.user.js
+++ TangerineUI.user.js
@@ -6,12 +6,13 @@
 // @match       https://piaille.fr/*
 // @match       https://eldritch.cafe/*
 // @match       https://h-i.social/*
+// @match       https://diaspodon.fr/*
 // @match       https://AddYourInstanceUrlHere.tld/*
 // @downloadURL https://github.com/Write/TangerineUI-Userscript/raw/main/TangerineUI.user.js
 // @updateURL   https://github.com/Write/TangerineUI-Userscript/raw/main/TangerineUI.user.js
 // @homepageURL https://github.com/Write/TangerineUI-Userscript
 // @grant       none
-// @version     2.2.3
+// @version     2.2.3.1
 // @author      @Write on Github for the UserScript
 // @author      @nileane for TangerineUI's CSS
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACE0lEQVR4AZXOA2xeUQAF4BvNW8xGi7WFY227v81otu0Fs23btq3attunvrN7//bOPMmX98414bFa4edyda12u7te0m8d1UKhRyNVT32idrtcGE6+jdMpBzjtUjOFf2KTZJddchKWsWPR22HprKLATBwnYOM6CfNni+Bjs2d0j02ZKLDOCVZrhx9xGNuT7aY2MAvmdKCjQwXP0UMidm4ToPYMiSKwcmkn+Hqbsd1L7LqWqTZ9M5injyXf4oN7O3HkQCdkGRAEFWdOCNi9vROKAnx8L4Ovt+uaZhCbpn6eTVsPpjBfRkN9F3ivLFfQ0qJ+6SXFCupqv86zvcSWVjPTml4DprhQRm21At7ZgbU1X3tBnoxGegHvlvSa2cSaUjXWklIJhh/Ae2Ge5DuA94JcyfdC3q2plVOJObncYU4qA1NcIPkO4H2soxLjXZW80wNEesDXeVNSqYcY44s1pvhiMDcvt+LB7Tbw/qPrF1rx+F77l25IKh1KjNEFw40xBfhfhpiCs4RFE/JxgDEqR6Xwj5qptZaot/0JjyE867UhPBOcMeJThCH8U6QhLNNgjMj06iMyjfQ/0RSROSQ2Nrc3+TG60I/J+tAPKgWfsI/7NaMe9SXfxBSbO4j8Kdrgdx5d0FuBQrd3zbrgN28Y2quofPK3aILeDtb6v1yt8X/5Suv/qo0Sdf6vijT+r05q/F9Hkl/kM/y+SpUd6mHfAAAAAElFTkSuQmCC
@@ -31,7 +32,7 @@
 
     /* colorScheme used for new (>= 4.3) instances */
     /* Either 'tangerine', 'purple', 'cherry' or 'lagoon' */
-    const colorScheme = 'tangerine';
+    const colorScheme = 'cherry';
 
     /* Github tag to use for mastodon instance < 4.3 and >= 4.3
     /* To find tags name, go here : https://github.com/nileane/TangerineUI-for-Mastodon/releases
@@ -98,12 +99,27 @@
     let isVersionDetected = false;
     let nonce;
 
+    function isVersionAtLeast(version, minVersion) {
+        const parseVersion = (v) => v.split('.').map(num => parseInt(num, 10));
+        const current = parseVersion(version);
+        const minimum = parseVersion(minVersion);
+
+        for (let i = 0; i < Math.max(current.length, minimum.length); i++) {
+            const currentPart = current[i] || 0;
+            const minimumPart = minimum[i] || 0;
+
+            if (currentPart > minimumPart) return true;
+            if (currentPart < minimumPart) return false;
+        }
+        return true; // Equal versions
+    }
+
     /* Version detection */
     onElemAvailable('script[id^=initial-state]').then((selector) => {
         isVersionDetected = true;
         const mastodonVersion = JSON.parse(selector.innerText).meta.version
         log("Mastodon Version Detected is : " + mastodonVersion);
-        if (mastodonVersion.includes('4.3.') || mastodonVersion.includes('4.4.') || mastodonVersion.includes('4.5.')) {
+        if (isVersionAtLeast(mastodonVersion, '4.3.0')) {
             log("Version above or equal 4.3.0 found.")
             if (isPurple)
               styleUrl = tangerine_above_or_equals_4_3_0 + "-purple.min.css"
@@ -170,7 +186,7 @@
                       isVersionDetected = true;
                       const mastodonVersion = JSON.parse(node.innerText).meta.version
                       log("(mutationObserver) Mastodon Version Detected is : " + mastodonVersion);
-                      if (mastodonVersion.includes('4.3.') || mastodonVersion.includes('4.4.') || mastodonVersion.includes('4.5.')) {
+                      if (isVersionAtLeast(mastodonVersion, '4.3.0')) {
                           log("(mutationObserver) Version above or equal 4.3.0 found.")
                           if (isPurple)
                             styleUrl = tangerine_above_or_equals_4_3_0 + "-purple.min.css"
@@ -190,11 +206,11 @@
                         href = node.href;
                         const rChunk = /\/packs\/css\/mastodon.*\.chunk\.css/
                         const rCustom = /custom\.css/
-
-                        /* Inject after custom.css or mastodon.*.chunk.css, whichever is detected first
-                         * On my system mastodon.*.chunk.css wasn't detected on Chrome
-                         * */
-                        if ((href.match(rCustom) || href.match(rChunk)) && !isInjected) {
+                        const rCustomWithHash = /custom-[a-f0-9]+\.css/
+                        /* Inject after custom.css (old or new format) or mastodon.*.chunk.css, whichever is detected first
+         * On my system mastodon.*.chunk.css wasn't detected on Chrome
+         */
+                        if ((href.match(rCustom) || href.match(rCustomWithHash) || href.match(rChunk)) && !isInjected) {
                             if (nonce && isVersionDetected) {
                                 nChanges++;
                                 isInjected = true;
@@ -248,22 +264,24 @@
     /* Workaround if MutationObserver couldn't detect the item
      * Usually in Safari and Chrome (for Chrome, the first load seems to use MutationObserver, but anyway)
      * */
-    onElemAvailable('link[rel^=stylesheet][href*=custom\\.css]').then(() => {
-        if (!isInjected) {
-            if (nonce) {
-                document.head.appendChild(createStyleNode(nonce));
-                isInjected = true;
-                log("(requestAnimationFrame) custom.css detected, injected TangerineUI's css asap");
-            }
-            else {
-                isInjected = true;
-                onElemAvailable('meta[name^=style\\-nonce]').then((selector) => {
-                    let nonce_fallback = selector.content;
-                    document.head.appendChild(createStyleNode(nonce_fallback));
-                    log("Nonce was lookedup with requestAnimationFrame as MutationObserver didn't catch it.");
-                    log('Nonce found is ' + nonce_fallback);
+    onElemAvailable('link[rel="stylesheet"][href*="custom"]').then((elem) => {
+        if (elem.href.includes('custom.css') || elem.href.match(/custom-[a-f0-9]+\.css/)) {
+            if (!isInjected) {
+                if (nonce) {
+                    document.head.appendChild(createStyleNode(nonce));
+                    isInjected = true;
                     log("(requestAnimationFrame) custom.css detected, injected TangerineUI's css asap");
-                });
+                }
+                else {
+                    isInjected = true;
+                    onElemAvailable('meta[name^=style\\-nonce]').then((selector) => {
+                        let nonce_fallback = selector.content;
+                        document.head.appendChild(createStyleNode(nonce_fallback));
+                        log("Nonce was lookedup with requestAnimationFrame as MutationObserver didn't catch it.");
+                        log('Nonce found is ' + nonce_fallback);
+                        log("(requestAnimationFrame) custom.css detected, injected TangerineUI's css asap");
+                    });
+                }
             }
         }
     });
